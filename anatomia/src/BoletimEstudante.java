d127b176e6ad1a0b37784ef4552f1ce031801202
Vitório Corrêa <vitcorreapereira@hotmail.com>
2024-09-18 21:22:02 -0300

    Boletim Estudante

---
+++ anatomia/src/BoletimEstudante.java
@@ -0,0 +1,12 @@
+public class BoletimEstudante {
+    
+    public static void main(String[] args){
+        int mediaFinal = 7;
+        if(mediaFinal< 6)
+        System.out.println("REPORVADO");
+        else if(mediaFinal == 6)
+        System.out.println("PROVA MINERVA");
+        else
+        System.out.println("Aprovado"); 
+    }
+}
