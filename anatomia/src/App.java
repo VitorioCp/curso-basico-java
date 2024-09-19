edfce339207ef1fe4c6fe1a60687c170efc89595
Vitório Corrêa <vitcorreapereira@hotmail.com>
2024-09-19 18:46:36 -0300

    Argumentos

--- anatomia/src/App.java
+++ anatomia/src/App.java
@@ -1,11 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        String primeiroNome = "Vitório";
-        String segundoNome = "Corrêa";
+        // String primeiroNome = "Vitório";
+        // String segundoNome = "Corrêa";
 
-        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
-        System.out.println("Bem vindo " + nomeCompleto + "!");
+        // String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        // System.out.println("Bem vindo " + nomeCompleto + "!");
+
+        SmartTv smartTv = new SmartTv();
+    
+        System.out.println(smartTv.ligada);
+        System.out.println(smartTv.canal);
+        System.out.println(smartTv.volume);
+
+        smartTv.aumentarVolume();
+        System.out.println(smartTv.volume);
+
     }
 
     public static String nomeCompleto(String primeiroNome, String segundoNome) {
